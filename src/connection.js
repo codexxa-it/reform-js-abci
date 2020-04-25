f3ea691915fae0871e31a90d7b17e9041ee5e48b
Kabir Khaleque <kabir@divm.com>
2020-04-25 23:46:35 +0200

    fix for cannot decode varint

--- src/connection.js
+++ src/connection.js
@@ -39,43 +39,48 @@ class Connection extends EventEmitter {
   }
 
   maybeReadNextMessage () {
-    const length = varint.decode(this.recvBuf.slice(0, 8)) >> 1
-    const lengthLength = varint.decode.bytes
-
-    if (length > MAX_MESSAGE_SIZE) {
-      this.error(Error('message is longer than maximum size'))
-      return
-    }
-
-    if (lengthLength + length > this.recvBuf.length) {
-      // buffering message, don't read yet
-      return
-    }
-
-    const messageBytes = this.recvBuf.slice(
-      lengthLength,
-      lengthLength + length
-    )
-    this.recvBuf.consume(lengthLength + length)
-
-    const message = Request.decode(messageBytes)
-
-    this.waiting = true
-    this.stream.pause()
-
-    // log incoming messages, except for 'flush'
-    if (!message.flush) {
-      debug('<<', message)
-    }
-
-    this.onMessage(message, () => {
-      this.waiting = false
-      this.stream.resume()
-
-      if (this.recvBuf.length > 0) {
-        this.maybeReadNextMessage()
+    try {
+      const length = varint.decode(this.recvBuf.slice(0, 8)) >> 1
+      const lengthLength = varint.decode.bytes
+    
+      if (length > MAX_MESSAGE_SIZE) {
+        this.error(Error('message is longer than maximum size'))
+        return
       }
-    })
+
+      if (lengthLength + length > this.recvBuf.length) {
+        // buffering message, don't read yet
+        return
+      }
+
+      const messageBytes = this.recvBuf.slice(
+        lengthLength,
+        lengthLength + length
+      )
+      this.recvBuf.consume(lengthLength + length)
+
+      const message = Request.decode(messageBytes)
+
+      this.waiting = true
+      this.stream.pause()
+
+      // log incoming messages, except for 'flush'
+      if (!message.flush) {
+        debug('<<', message)
+      }
+
+      this.onMessage(message, () => {
+        this.waiting = false
+        this.stream.resume()
+
+        if (this.recvBuf.length > 0) {
+          this.maybeReadNextMessage()
+        }
+      })
+    } catch (e) {
+      console.error(e);
+      return;
+    }
   }
 
   write (message) {
